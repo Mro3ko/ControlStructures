bd0d5ecb2e6b54d0a4da3f098f2017118084bb91
Mateusz Mroczka <mateusz.mroczka7@gmail.com>
2024-11-22 17:02:41 +0100

    pierwszy commit

---
+++ speed_limit.py
@@ -0,0 +1,5 @@
+speed_limit = 140
+car_speed = int( input('Enter car speed (km/h): ') )
+if car_speed >140:
+    print(f'Your speed is {speed_limit}km/h')
+    print('Warning: speed limit exceeded!!')
